d3892681f9a30e045668d6b99157eb0e8060fc8f
rheomie <rheomie@yahoo.com>
2014-10-21 13:29:54 +0700

    1st commit untuk tugas perulangan

---
+++ src/id/blits/latihan/ActivityClass.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.blits.latihan;
+
+import id.blits.util.DataMahasiswa;
+
+
+/**
+ *
+ * @author Lintanhg
+ */
+public class ActivityClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Data rio = new Data();
+        rio.kelas = "TI12K";
+        System.out.print(rio.kelas);
+        
+        DataMahasiswa sanriomi = new DataMahasiswa();
+        
+        sanriomi.
+    }
+    
+}
---
+++ src/id/blits/util/DataMahasiswa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.blits.util;
+
+/**
+ *
+ * @author Lintanhg
+ */
+public class DataMahasiswa {
+  boolean menikah = true;
+  
+  System.out.print(menikah);
+            
+}
